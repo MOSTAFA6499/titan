5ed6c9fb10d303bf2837727c60afa414d01532fd
MOSTAFA6499 <mostafazarei9292@gmail.com>
2026-06-06 04:20:05 +0330

    Create passenger_wsgi.py

---
+++ passenger_wsgi.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+# مسیر هاست - اگه بعداً مشکل داشت این رو عوض کن
+PROJECT_PATH = '/home/mostafazareii/public_html/titan_system'
+
+if PROJECT_PATH not in sys.path:
+    sys.path.insert(0, PROJECT_PATH)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'titan_config.settings')
+os.environ['PYTHON_EGG_CACHE'] = '/tmp/python_egg_cache'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
