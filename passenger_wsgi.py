afdc44d24756b5648c1d45a07e8c99b14b236693
MOSTAFA6499 <mostafazarei9292@gmail.com>
2026-06-06 06:33:05 +0330

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,6 @@
 import os
 import sys
 
-# مسیر ریشه پروژه
-sys.path.insert(0, os.path.dirname(__file__))
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'titan_config.settings')
 
 from django.core.wsgi import get_wsgi_application
