3f64532520b73c178f70fa3d9afbae5e47428d25
MOSTAFA6499 <mostafazarei9292@gmail.com>
2026-06-06 06:13:20 +0330

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,10 @@
 import os
 import sys
 
-# 🔴 این آدرس رو به مسیر واقعی هاستت تغییر بده
-# (از File Manager ببین دقیقاً کجایی)
-PROJECT_PATH = '/home/mostafazarei/public_html'
-
-if PROJECT_PATH not in sys.path:
-    sys.path.insert(0, PROJECT_PATH)
+# مسیر ریشه پروژه
+sys.path.insert(0, os.path.dirname(__file__))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'titan_config.settings')
-os.environ['PYTHON_EGG_CACHE'] = '/tmp/python_egg_cache'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
